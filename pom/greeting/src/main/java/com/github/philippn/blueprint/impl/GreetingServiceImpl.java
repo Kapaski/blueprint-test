6ad09dbe076f4696bb07bfc62c42ccacc9f2532a
Philipp Nanz <philippn@gmail.com>
2011-10-05 23:10:39 +0200

    added logging and removed dependency to direct gemini blueprint

--- pom/greeting/src/main/java/com/github/philippn/blueprint/impl/GreetingServiceImpl.java
+++ pom/greeting/src/main/java/com/github/philippn/blueprint/impl/GreetingServiceImpl.java
@@ -1,11 +1,23 @@
 package com.github.philippn.blueprint.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.philippn.blueprint.IGreetingService;
 
 /**
  * @author Philipp
  */
 public class GreetingServiceImpl implements IGreetingService {
+
+	private Logger logger = LoggerFactory.getLogger(GreetingServiceImpl.class);
+
+	/**
+	 * This method is called by the Blueprint subsystem.
+	 */
+	public void setup() {
+		logger.info("Initializing Greeting service...");
+	}
 
 	/* (non-Javadoc)
 	 * @see com.github.philippn.blueprint.IGreetingService#greet(java.lang.String)
